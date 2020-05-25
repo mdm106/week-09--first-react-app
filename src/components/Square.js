dc5b6f44e6a920c64347f5eb87910361b3490de6
mdm106 <mads.mcmahon@googlemail.com>
2020-05-25 15:07:58 +0100

    Square component updated to accept a prop called colour, and a default colour added

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,14 +1,12 @@
 import React from "react";
 
-const divStyle = {
-    backgroundColor: "red",
-    width: 200,
-    height: 200,
-};
-
-const Square = () => (
-    <div style={divStyle}>
+const Square = ({ colour }) => (
+    <div style={{ width: 200, height: 200, backgroundColor: colour}}>
     </div>
 );
+
+Square.defaultProps = {
+    colour: "rgb(0,128,128)"
+};
 
 export default Square;
