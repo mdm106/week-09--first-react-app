965bba18deea7e101163312eade1af2daa34d0c7
mdm106 <mads.mcmahon@googlemail.com>
2020-05-25 15:02:15 +0100

    Paragraph component updated to accept a message prop which will display between p tags, and will display Hello, World if no message prop is provided

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,8 +1,10 @@
 //import the React library
 import React from "react";
 
-const Paragraph = () => (
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos.</p>
+const Paragraph = ({ message }) => (
+    <>
+        <p>{!message ? "Hello, World" : message}</p>
+    </>
 );
 
 export default Paragraph;
