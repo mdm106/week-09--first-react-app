27f673a7ae4da4cbb80c1880da4707286fbccb52
mdm106 <mads.mcmahon@googlemail.com>
2020-05-25 12:52:44 +0100

    People component created, which outputs a ul with li populated using an array

---
+++ src/components/People.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+let names = ["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"];
+
+const People = () => (
+    <ul>
+        {names.map((name, index) => (
+            <li key= { index }>
+                { name }
+            </li>
+        ))}
+    </ul>
+);
+
+export default People;
