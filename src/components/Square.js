2ab930dd80f75efc2f818957c4f1a7954ba04dd0
mdm106 <mads.mcmahon@googlemail.com>
2020-05-26 14:21:01 +0100

    Square component updated so that background starts green, but when it is clicked it changes to the passed in colour (or the default if no colour passed in). Alternates every click

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Square = ({ colour }) => (
-    <div style={{ width: 200, height: 200, backgroundColor: colour}}>
-    </div>
-);
+class Square extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { clicker: true };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        let currentClicker = this.state.clicker;
+
+        this.setState({ clicker: !currentClicker });
+    }
+
+    render() {
+        const { colour } = this.props;
+        const { clicker } = this.state;
+
+        return (
+            <div onClick={this.handleClick} style={{ width: 200, height: 200, backgroundColor: clicker ? "green" : colour}}>
+            </div>
+        )
+    }
+};
 
 Square.defaultProps = {
-    colour: "rgb(0,128,128)"
+    colour: "hotpink"
 };
 
 export default Square;
