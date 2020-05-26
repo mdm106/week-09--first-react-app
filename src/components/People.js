c89b1e71125c798c297f088741d62a9924209970
mdm106 <mads.mcmahon@googlemail.com>
2020-05-26 12:28:22 +0100

    Updated People component so that when the ul is clicked 'Hello, world' gets logged in the console

--- src/components/People.js
+++ src/components/People.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
-const People = ({ names }) => (
-    names ? (
-        <ul>
-            {names.map((name, index) => (
-                <li key= { index }>
-                    { name }
-                </li>
-            ))}
-        </ul>
-    ) : <p>Nothing to see here</p> 
-);
+class People extends Component {
+
+    handleClick() {
+        console.log("Hello, world");
+    }
+
+    render () {
+        let { names } = this.props;
+
+        return (
+            names ? (
+                <ul onClick={ this.handleClick }>
+                    {names.map((name, index) => (
+                        <li key= { index }>
+                            { name }
+                        </li>
+                    ))}
+                </ul>
+            ) : <p>Nothing to see here</p> 
+        )
+    }
+
+}
 
 export default People;
