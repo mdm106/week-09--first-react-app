99eb52cb4d4e7e48600fda63d28cf75ab6b4983a
mdm106 <mads.mcmahon@googlemail.com>
2020-05-28 09:27:51 +0100

    Restyling to improve layout

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -28,8 +28,10 @@ class Counter extends Component {
         return (
             <>
                 <p>Counter: {counter} </p>
-                <button className="btn-success" onClick={this.handleClickPlus}>+</button>
-                <button className="btn-danger" onClick={this.handleClickMinus}>-</button>
+                <div className="d-flex flex-row">
+                    <button className="btn-success" onClick={this.handleClickPlus}>+</button>
+                    <button className="btn-danger" onClick={this.handleClickMinus}>-</button>
+                </div>
             </>
         )
     }
