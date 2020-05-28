4a953dda72a004dc357d7c005b3696fe78c4bb51
mdm106 <mads.mcmahon@googlemail.com>
2020-05-28 10:47:23 +0100

    Components moved from App to Stuff component, and stuff component included in App

--- src/components/Stuff.js
+++ src/components/Stuff.js
@@ -4,6 +4,21 @@ import Header from "./Header";
 import Paragraph from "./Paragraph";
 import Square from "./Square";
 import People from "./People";
+import Basket from './Basket';
+import ToggleText from './ToggleText'
+import Counter from './Counter'
+import StepCounter from './StepCounter'
+import CatchMeIfYouCan from './CatchMeIfYouCan';
+import RollCall from './RollCall';
+import Colours from './tricksyState/Colours';
+import Die from './tricksyState/Die';
+import LameGame from './tricksyState/LameGame';
+import Length from './formComponents/Length';
+import PasswordStrength from './formComponents/PasswordStrength';
+import TempConverter from './formComponents/TempConverter';
+import List from './formComponents/List';
+import Adder from './formComponents/Adder';
+import Transform from './formComponents/Transform';
 
 const Stuff = ({ square }) => (
     <>
@@ -15,6 +30,30 @@ const Stuff = ({ square }) => (
         </Paragraph>
         {square ? <Square /> : null}
         <People names = {["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"]}/>
+        <Basket items={
+          [
+            { name: "Coffee", price: 2.10 },
+            { name: "Bananas", price: 3.50 },
+            { name: "Milk", price: 250.65 },
+            { name: "The Great Milk Shortage by Simon Schama", price: 12.99 },
+          ]
+        } />
+        <ToggleText />
+        <Counter />
+        <StepCounter />
+        <CatchMeIfYouCan />
+        <RollCall />
+        <Colours />
+        <Die />
+        <LameGame aim={5}/>
+        <Length />
+        <PasswordStrength />
+        <TempConverter />
+        <List />
+        <Adder />
+        <Transform transform={x => x * x} />
+        <Transform transform={x => x + 4} />
+        <Transform transform={x => x * 10} />
     </>
 );
 
