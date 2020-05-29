05943ca08cc1b67b254a04361b04dfa4f7470c79
mdm106 <mads.mcmahon@googlemail.com>
2020-05-29 16:21:23 +0100

    Updates

---
+++ precache-manifest.3558f946342dc0b8a52c8a1e98b24dc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd43a6e814d353f0d9d9098aab3fb7cb",
+    "url": "/week-09--first-react-app/index.html"
+  },
+  {
+    "revision": "72b862466486cc2ea4ca",
+    "url": "/week-09--first-react-app/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "5c795e685b2d9445c56e",
+    "url": "/week-09--first-react-app/static/css/main.0a2098c1.chunk.css"
+  },
+  {
+    "revision": "72b862466486cc2ea4ca",
+    "url": "/week-09--first-react-app/static/js/2.d80c5651.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/week-09--first-react-app/static/js/2.d80c5651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c795e685b2d9445c56e",
+    "url": "/week-09--first-react-app/static/js/main.1fc89360.chunk.js"
+  },
+  {
+    "revision": "c54bcc25c432536efded",
+    "url": "/week-09--first-react-app/static/js/runtime-main.66966d47.js"
+  }
+]);
